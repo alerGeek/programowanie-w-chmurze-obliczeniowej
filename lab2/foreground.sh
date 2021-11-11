7dccd97c0d458f5a0880fd36f26860c5a1700721
alergeek <promucha@gmail.com>
2021-11-11 19:48:07 +0100

    change foreground

--- lab2/foreground.sh
+++ lab2/foreground.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 
 apt-get update
+echo '[INFO] Configuring lxc...'
 apt-get -y install lxc lxc-utils lxc-templates
+echo '[INFO] Finished configuring lxc.'
+
+echo '[INFO] Configuring lxd...'
 apt-get -y install lxd
+echo '[INFO] Finished configuring lxd.'
