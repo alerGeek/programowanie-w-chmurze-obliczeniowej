d4aef2ca269d3425a8a3b402aef2255bdbb0c42d
alergeek <promucha@gmail.com>
2021-11-11 20:36:24 +0100

    change foreground

--- lab2/foreground.sh
+++ lab2/foreground.sh
@@ -1,10 +1,10 @@
-if sudo apt-get -y install lxd; then
+if sudo apt-get -y install lxd lxc; then
     echo 'LXD zainstalowano pomyślnie'
 else
     echo 'LXD rzuciło error'
 fi
 
-if sudo apt-get -y install lxc; then
+if sudo apt-get -y install c; then
     echo 'LXD zainstalowano pomyślnie'
 else
     echo 'LXD rzuciło error'
