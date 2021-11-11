7ad9d8cd8035d3f1335a716aef71899be8d112a8
alergeek <promucha@gmail.com>
2021-11-11 23:04:51 +0100

    change foreground

--- lab2/background.sh
+++ lab2/background.sh
@@ -1,2 +1,11 @@
 apt-get update
+
+while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
+
+if sudo apt-get -y install lxd lxc1 lxc; then
+    echo 'Środowisko skonfigurowano pomyślnie'
+else
+    echo 'ERROR. Sprawdź konfigurację środowiska'
+fi
+
 echo "done" >> /opt/.backgroundfinished
--- lab2/foreground.sh
+++ lab2/foreground.sh
@@ -1,8 +1,4 @@
 echo "Waiting to complete"
-while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
 
-if sudo apt-get -y install lxd lxc1 lxc; then
-    echo 'Środowisko skonfigurowano pomyślnie'
-else
-    echo 'ERROR. Sprawdź konfigurację środowiska'
-fi
+lxc-checkconfig
+ifconfig lxcbr0
